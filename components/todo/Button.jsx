5ec3726802a52d9accdc031f41be38caca080239
Ekaterina-timans <128321173+Ekaterina-timans@users.noreply.github.com>
2023-10-01 21:03:01 +0300

    Add page To Do List

---
+++ components/todo/Button.jsx
@@ -0,0 +1,7 @@
+import { memo } from 'react';
+
+
+export default memo(function Button({onClick, children}) {
+    console.debug('Button render');
+    return <button onClick={onClick}>{children}</button>
+  });
---
+++ components/todo/index.jsx
@@ -0,0 +1,24 @@
+import { useState, useCallback, useRef } from "react";
+import Form from "./Form";
+import List from "./List";
+import { createItem } from "./Item";
+
+export default function ToDoApp() {
+    const 
+        [list, setList] = useState([createItem('111'), createItem('222')]),
+        addItem = useCallback(text => setList(old => old.concat(createItem(text))), []),
+        delItem = useCallback(id => setList(old => old.filter(item => id!==item.id)), []),
+        ref = useRef([]),
+        toggleCheckbox = useCallback(id => {
+            const
+                index = ref.current.findIndex(item => id === item.id),
+                {checked, ...rest} = ref.current[index];
+            setList(ref.current.with(index, {...rest, checked: !checked}));
+        }, []);
+
+    ref.current = list;
+    return <>
+        <Form addItem={addItem}/>
+        <List list={list} delItem={delItem} toggleCheckbox={toggleCheckbox}/>
+      </>
+}
